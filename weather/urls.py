5b19c09e060b0fcc5aba4da4de14283ddf7dc5d4
Robee <rob93ghosh@gmail.com>
2021-05-28 22:27:26 +0400

    added viewsets

--- weather/urls.py
+++ weather/urls.py
@@ -20,29 +20,17 @@ from geoCodes.models import GeogInfo
 from geoCodes.serializers import GeogInfoSerializer
 from rest_framework import routers, serializers, viewsets
 
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-class GeogInfoViewSet(viewsets.ModelViewSet):
-    queryset = GeogInfo.objects.all()
+class GeogInfoViewSet(viewsets.ReadOnlyModelViewSet): 
+    queryset = GeogInfo.objects.filter(timezone='Asia/Dubai')
     serializer_class = GeogInfoSerializer
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
 router.register(r'GeogInfo',GeogInfoViewSet)
 # router.register(r'weatherapi', geoData)
 
 urlpatterns = [
     path('admin/', admin.site.urls),#username:admin password:admin
     path('wapi/',include('geoCodes.urls')),
-     path('routers/', include(router.urls)),
+    path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls'))
 ]
